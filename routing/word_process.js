7fa7f9bb66a09cbd0f1462041623ccf074eb0849
mohan1497 <mohankrishnapechetti@gmail.com>
2019-12-03 16:57:30 +0530

    code modyfied on error

--- routing/word_process.js
+++ routing/word_process.js
@@ -19,8 +19,7 @@ module.exports = class word_process{
 					}
 					else{
 						resolve(resp["body"]);	
-					}
-					
+					}					
 				});
         	})
 		}
@@ -32,53 +31,70 @@ module.exports = class word_process{
 	async parsing_word(word,opt){
 		var def_apihost = `${this.apihost}/word/${word}/${config[opt]}?api_key=${this.api_key}`;
 		let obj ={}
-		var result =await this.getting_from_api(def_apihost,opt);		
-		if(result.hasOwnProperty('error')){
-			return result["error"];
+		try{
+			var result =await this.getting_from_api(def_apihost,opt);		
+			if(result.hasOwnProperty('error')){
+				return result["error"];
+			}
+			else{
+				if(opt == 'def'){
+					obj[opt] = result.map(obj => obj["text"]);
+				}
+				else if(opt == 'syn' || opt == 'ant' || opt == 'word'){
+				 	result.forEach((element)=>{ obj[element["relationshipType"].slice(0,3)]=element["words"] });
+				}
+				else if(opt == 'ex'){
+					obj[opt] = result["examples"].map(obj => obj["text"])
+				}
+				return obj;
+			}
 		}
-		else{
-			if(opt == 'def'){
-				obj[opt] = result.map(obj => obj["text"]);
-			}
-			else if(opt == 'syn' || opt == 'ant' || opt == 'word'){
-				 result.forEach((element)=>{ obj[element["relationshipType"].slice(0,3)]=element["words"] });
-			}
-			else if(opt == 'ex'){
-				obj[opt] = result["examples"].map(obj => obj["text"])
-			}
-			return obj;
+		catch(ex){
+			console.log(ex)
 		}
-
 	}
 
 	async  word_full_dict (word) {
             var _this = this;
-            let ar_opt = config["options"];     
-            const promises = ar_opt.map(each_opt => _this.parsing_word(word,each_opt))
-            const result = await Promise.all(promises);
-            let result_obj={}
-            result.forEach(obj => {
-            	let keys = Object.keys(obj)
-            	if(keys.length>1){
-            		result_obj[keys[0]]=obj[keys[0]]
-            		result_obj[keys[1]]=obj[keys[1]]
-            	}
-            	else{
-            		let key = keys.toString()
-            		result_obj[key]=obj[key]
-            	}  
-			})
-            return result_obj;
+            try{
+            	let ar_opt = config["options"];     
+            	const promises = ar_opt.map(each_opt => _this.parsing_word(word,each_opt))
+            	const result = await Promise.all(promises);
+            	if(typeof result[0] == 'string')
+            		return result[0]
+            	let result_obj={}
+            	result.forEach(obj => {
+            		let keys = Object.keys(obj)
+            		if(keys.length>1){
+            			result_obj[keys[0]]=obj[keys[0]]
+            			result_obj[keys[1]]=obj[keys[1]]
+            		}
+            		else{
+            			let key = keys.toString()
+            			result_obj[key]=obj[key]
+            		}  
+				})
+            	return result_obj;
+            }
+            catch(ex){
+            	console.log(ex)
+            }
     }
-	// word_of_the_day_full_dict method fetch random word from api 
+	// word_of_the_day_full_dict method fetch random word from api and
 
 	async word_of_the_day_full_dict(){
-		var random_apihost = `${this.apihost}/words/randomWord?api_key=${this.api_key}`;
-		var random_word = await this.getting_from_api(random_apihost);
-		var random_full_dict = await this.word_full_dict(random_word["word"])
-		var result_data ={"random_word":random_word["word"],"full_dict":random_full_dict}
-		return result_data;
+		try{
+			var random_apihost = `${this.apihost}/words/randomWord?api_key=${this.api_key}`;
+			var random_word = await this.getting_from_api(random_apihost);
+			var random_full_dict = await this.word_full_dict(random_word["word"])
+			var result_data ={"random_word":random_word["word"],"full_dict":random_full_dict}
+			return result_data;
+		}
+		catch(ex){
+			console.log(ex)
+		}
 	}
+
 	display_result(result,opt){
 
 		if (typeof result == "string")
@@ -89,6 +105,7 @@ module.exports = class word_process{
 		else
 			console.log(opt,': Not Found')
 	}
+	
 	process_full_dict(result){
 		var _this = this;
 		let keys = Object.keys(result)
@@ -101,21 +118,27 @@ module.exports = class word_process{
 	async process_word(options){
 		var _this = this;
 		var opt = options["opt"]
-		if (opt == 'word'){
-			let result = await  _this.word_full_dict(options["word"]);
-			_this.process_full_dict(result)		
+		try{
+			if (opt == 'word'){
+				let result = await  _this.word_full_dict(options["word"]);
+				if(typeof result == 'string')
+					console.log(result)
+				else
+					_this.process_full_dict(result)		
+			}
+			else if(opt == 'random'){
+				let result = await  _this.word_of_the_day_full_dict();			
+				console.log("Word of The Day: ",result["random_word"]+'\n')
+				_this.process_full_dict(result["full_dict"])
+			}
+			else{
+				let result = await _this.parsing_word(options["word"],opt);
+				_this.display_result(result,opt)	
+			}		
 		}
-		else if(opt == 'random'){
-			let result = await  _this.word_of_the_day_full_dict();			
-			console.log("Word of The Day: ",result["random_word"]+'\n')
-			_this.process_full_dict(result["full_dict"])
+		catch(ex){
+			console.log(ex)
 		}
-		else{
-			let result = await _this.parsing_word(options["word"],opt);
-			_this.display_result(result,opt)	
-		}
-	
-		
 	}
 }
 
